8e5f4d3df9fbac157fe561d4d8d2cdb5b68aed03
lavraceae <laurabernabeuarmas@gmail.com>
2025-08-03 22:55:50 +0100

    Make a collection

--- src/content.config.ts
+++ src/content.config.ts
@@ -8,12 +8,8 @@ const blog = defineCollection({
   schema: z.object({
     title: z.string(),
     pubDate: z.date(),
-    description: z.string(),
     author: z.string(),
-    image: z.object({
-      url: z.string(),
-      alt: z.string()
-    }),
+    ordinal: z.string(),
     tags: z.array(z.string())
   })
 });
